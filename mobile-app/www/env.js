98a398b165a042906c3ff20e41e384589d185b6f
filaos974 <filaos974@hotmail.com>
2024-09-09 13:10:51 +0400

    traduction + sup commentaires + modif css

--- mobile-app/www/env.js
+++ mobile-app/www/env.js
@@ -1,8 +1,7 @@
 export const env = {
   mode_nfc: "NFCMC",
   front_type: "FMO",
-  server_pin_code: "https://discovery.filaos.re",
-  // server_pin_code: "https://discovery.tibillet.coop",
+  server_pin_code: "https://discovery.tibillet.coop",
   servers: [],
   client: null,
   current_server: ""
